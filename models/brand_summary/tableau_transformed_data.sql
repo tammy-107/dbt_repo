ad437e5fc842cfdb9887ca53626f377544c49af8
tammy tammy <mailtammy05@gmail.com>
2025-02-25 11:20:24 +0000

    gt

--- models/brand_summary/tableau_transformed_data.sql
+++ models/brand_summary/tableau_transformed_data.sql
@@ -4,4 +4,4 @@ SELECT *,
         WHEN PA_OUTCOME ILIKE '%Denied%' THEN 'Denied' 
         ELSE 'None'
     END AS pa_actual_outcome
-FROM {{ source('brand_summary', 'brand_summary') }}
+FROM {{ source('postgres_test', 'brand_summary') }}
