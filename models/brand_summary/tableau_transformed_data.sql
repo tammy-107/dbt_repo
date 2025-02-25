ca96c10acee853d64c3e23f7020c90a388a87e00
tammy tammy <mailtammy05@gmail.com>
2025-02-25 13:47:31 +0000

    dwfwecfew

--- models/brand_summary/tableau_transformed_data.sql
+++ models/brand_summary/tableau_transformed_data.sql
@@ -6,5 +6,6 @@ select
         when pa_outcome ilike '%Denied%'
         then 'Denied'
         else 'None'
-    end as pa_actual_outcome
+    end as pa_actual_outcome,
+    UPPER(brand) as BRAND_NAME
 from {{ source("postgres_test", "brand_summary") }}
